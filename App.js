ef81af3b17a9bf64c779c21ecab782f5e0a4c57c
Anatolij Goga <anzefal@gmail.com>
2023-07-17 20:47:03 +0300

    Updated App.js
    Added fonts

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
 import React from "react";
-import RegistrationScreen from "./Screens/RegistrationScreen";
+import { useFonts } from "expo-font";
+import { useCallback } from "react";
+import RegistrationScreen from "./Screens/authentication/RegistrationScreen/RegistrationScreen";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+    "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+    "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <React.StrictMode>
-      <RegistrationScreen />
+      <RegistrationScreen onReady={onLayoutRootView} />
     </React.StrictMode>
   );
 }
